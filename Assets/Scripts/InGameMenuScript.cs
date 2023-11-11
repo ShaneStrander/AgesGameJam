e38283377de3ef3f5aea35165927f7077c9b338e
TheRealDemmy <Dendimir101@yahoo.com>
2023-11-11 16:52:06 +0100

    InGameMenuMade

--- Assets/Scripts/InGameMenuScript.cs
+++ Assets/Scripts/InGameMenuScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool GameMenuBool = false;
+
+    public GameObject BackgroundActivate;
+    public GameObject GameMenuActivate;
+
+    public void Update()
     {
-        
+        GameMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameMenu()
     {
-        
+        if (Input.GetKeyDown("escape") && GameMenuBool == false)
+        {
+            Debug.Log("yeehaw");
+            GameMenuActivate.SetActive(true);
+            BackgroundActivate.SetActive(true);
+            GameMenuBool = true;
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown("escape") && GameMenuBool == true)
+        {
+            GameMenuActivate.SetActive(false);
+            BackgroundActivate.SetActive(false);
+            GameMenuBool = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
