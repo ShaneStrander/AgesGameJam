45f9b0aee0eb8394a82cc4c1ef76f4ba15eb9f44
ShaneStrander <shanestrander@gmail.com>
2023-11-12 13:55:43 +0100

    add crane to main scene

--- Assets/Scripts/scr_crane.cs
+++ Assets/Scripts/scr_crane.cs
@@ -10,8 +10,21 @@ public class scr_crane : MonoBehaviour
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        mousePosition.y = transform.position.y;
+        // if in inventory
+        if (mousePosition.x > 12.5f)
+        {
+            //mousePosition.y = transform.position.y;
+            mousePosition.x = 16.5f;
+        }
+        else 
+        {
+            mousePosition.y = 9.8f;
+            //mousePosition.x = 7f;
+        }
+
+        //usePosition.y = transform.position.y;
         mousePosition.z = transform.position.z;
+        
 
         transform.position = Vector3.Lerp(transform.position, mousePosition, followSpeed * Time.deltaTime);
     }
