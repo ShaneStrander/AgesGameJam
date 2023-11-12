51089eefa0c1d5e9252a2699f38b12520f9f145b
ShaneStrander <shanestrander@gmail.com>
2023-11-12 03:09:50 -0600

    edited prefab variables

--- Assets/Scripts/scr_crane.cs
+++ Assets/Scripts/scr_crane.cs
@@ -8,14 +8,11 @@ public class scr_crane : MonoBehaviour
 
     void Update()
     {
-        // Get the current mouse position in world coordinates
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Set the Y position of the object to its current Y position
         mousePosition.y = transform.position.y;
         mousePosition.z = transform.position.z;
 
-        // Move the object towards the mouse position smoothly
         transform.position = Vector3.Lerp(transform.position, mousePosition, followSpeed * Time.deltaTime);
     }
 }
